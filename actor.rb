e5696282d617e7cfbd36bd8e2df309261cafa76e
MenTaLguY <mental@rydia.net>
2008-01-15 18:26:03 -0500

    move actors to stdlib

---
+++ actor.rb
@@ -0,0 +1,71 @@
+# depends on: class.rb
+#
+# actor.rb - implementation of the actor model
+#
+# Copyright 2007  MenTaLguY <mental@rydia.net>
+#
+# All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without 
+# modification, are permitted provided that the following conditions are met:
+# 
+# * Redistributions of source code must retain the above copyright notice,
+#   thi slist of conditions and the following disclaimer.
+# * Redistributions in binary form must reproduce the above copyright notice
+#   this list of conditions and the following disclaimer in the documentatio
+#   and/or other materials provided with the distribution.
+# * Neither the name of the Evan Phoenix nor the names of its contributors 
+#   may be used to endorse or promote products derived from this software 
+#   without specific prior written permission.
+# 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+class Actor
+  alias_method :private_new, :new
+  alias_method :new, :spawn
+  private :private_new
+  
+  class << self
+    def spawn(&prc)
+      channel = Channel.new
+      Thread.new do
+        channel << current
+        prc.call
+      end
+      channel.receive
+    end
+
+    def current
+      Thread.current[:__current_actor__] ||= private_new(current_mailbox)
+    end
+
+    def current_mailbox
+      Thread.current[:__current_mailbox__] ||= Mailbox.new
+    end
+    private :current_mailbox
+
+    def receive(&prc)
+      current_mailbox.receive(&prc)
+    end
+  end
+
+  def initialize(mailbox)
+    @mailbox = mailbox
+  end
+
+  def send(value)
+    @mailbox.send value
+    self
+  end
+  alias_method :<<, :send
+end
