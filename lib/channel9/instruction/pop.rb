7807c8e9ced128bbcdfb5ca4f992240723e651ca
Megan Batty <megan@stormbrew.ca>
2011-06-07 13:30:42 -0600

    Missed earlier documentation commit.

--- lib/channel9/instruction/pop.rb
+++ lib/channel9/instruction/pop.rb
@@ -1,5 +1,12 @@
 module Channel9
   module Instruction
+    # pop
+    # ---
+    # Removes the top element from the stack.
+    #
+    # Takes one arbitrary input:
+    #  SP -> value
+    # After executing, the stack will no longer have that element on it.
     class POP < Base
       def initialize(stream)
         super(stream, 1, 0)
