a5e6eb662a62f60a15c490ba514153ec189d6bd2
Graham Batty <graham-storm-git@ript.net>
2011-06-13 04:45:52 -0600

    Adding simple test harness.

---
+++ simple_tests.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+Dir["simple_tests/*.rb"].each do |test|
+  if (match = test.match(%r{^simple_tests/([0-9]{3,3})\.(.+)\.rb$}))
+    num = match[1]
+    name = match[2]
+
+    output = `ruby -rubygems -Ilib -I../channel9/lib bin/c9.rb #{test}`
+    expected = File.read(test.gsub(%r{\.rb$}, ".out")) rescue ""
+    puts("#{num} #{name}: #{output == expected ? 'OK' : 'FAIL'}")
+
+    if (output != expected)
+      puts "Expected:"
+      puts "\t#{expected.gsub("\n", "\n\t")}"
+      puts "Got:"
+      puts "\t#{output.gsub("\n", "\n\t")}"
+    end
+  end
+end
