4d46a86ed0e0570f79e8847fc6dab94a21fd5a90
Graham Batty <graham-storm-git@ript.net>
2011-06-19 14:47:20 -0600

    Filled out exception class hierarchy.

--- environment/kernel/boot/exceptions.rb
+++ environment/kernel/boot/exceptions.rb
@@ -36,6 +36,33 @@ class Exception
   end
 end
 
-class StandardError < Exception; end
+class NoMemoryError < Exception; end
 
+class ScriptError < Exception; end
+class LoadError < ScriptError; end
+class NotImplementedError < ScriptError; end
+class SyntaxError < ScriptError; end
+
+class SignalException < Exception; end
+class Interrupt < Exception; end
+
+class StandardError < Exception; end
+class ArgumentError < StandardError; end
+class IOError < StandardError; end
+class EOFError < IOError; end
+class IndexError < StandardError; end
+class LocalJumpError < StandardError; end
+class NameError < StandardError; end
+class NoMethodError < NameError; end
+class RangeError < StandardError; end
+class FloatDomainError < RangeError; end
+class RegexpError < StandardError; end
 class RuntimeError < StandardError; end
+class SecurityError < StandardError; end
+class SystemCallError < StandardError; end
+class SystemStackError < StandardError; end
+class ThreadError < StandardError; end
+class TypeError < StandardError; end
+class ZeroDivisionError < StandardError; end
+
+class SystemExit < Exception; end
