770e80b7844a27cc37bb5437ec6eb93785d0051d
Graham Batty <graham-storm-git@ript.net>
2011-07-20 04:40:11 -0600

    Moved collect method to cpp file.

--- ext/memory_pool.cpp
+++ ext/memory_pool.cpp
@@ -22,4 +22,32 @@ namespace Channel9
 	{ 
 		m_roots.erase(root); 
 	}
+
+	void MemoryPool::collect()
+	{
+		m_in_gc = true;
+
+		//switch pools
+		m_cur_pool = !m_cur_pool;
+		m_cur_chunk = m_pools[m_cur_pool];
+
+		//clear this pool
+		Chunk * c = m_cur_chunk;
+		while(c){
+			c->m_used = 0;
+			c = c->m_next;
+		}
+
+		std::set<GCRoot*>::iterator it;
+		for (it = m_roots.begin(); it != m_roots.end(); it++)
+		{
+			(*it)->scan();
+		}
+
+		//copy stuff over?
+
+		m_in_gc = false;
+	}
+
+
 }
