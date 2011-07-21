018f29af27ad922d55f2bcbcd1ed85da877286b6
Timo Ewalds <timo@ewalds.ca>
2011-07-20 20:20:15 -0600

    Iterate over the new heap scanning everything to copy them into the new heap

--- ext/memory_pool.cpp
+++ ext/memory_pool.cpp
@@ -1,6 +1,14 @@
 #include "channel9.hpp"
 #include "memory_pool.hpp"
 
+#include "value.hpp"
+
+#include "string.hpp"
+#include "tuple.hpp"
+#include "message.hpp"
+#include "context.hpp"
+#include "variable_frame.hpp"
+
 namespace Channel9
 {
 	GCRoot::GCRoot(MemoryPool &pool)
@@ -14,13 +22,13 @@ namespace Channel9
 		m_pool.unregister_root(this);
 	}
 
-	void MemoryPool::register_root(GCRoot *root) 
-	{ 
-		m_roots.insert(root); 
+	void MemoryPool::register_root(GCRoot *root)
+	{
+		m_roots.insert(root);
 	}
-	void MemoryPool::unregister_root(GCRoot *root) 
-	{ 
-		m_roots.erase(root); 
+	void MemoryPool::unregister_root(GCRoot *root)
+	{
+		m_roots.erase(root);
 	}
 
 	void MemoryPool::collect()
@@ -32,10 +40,9 @@ namespace Channel9
 		m_cur_chunk = m_pools[m_cur_pool];
 
 		//clear this pool
-		Chunk * c = m_cur_chunk;
-		while(c){
+		for(Chunk * c = m_cur_chunk; c; c = c->m_next)
+		{
 			c->m_used = 0;
-			c = c->m_next;
 		}
 
 		std::set<GCRoot*>::iterator it;
@@ -44,10 +51,23 @@ namespace Channel9
 			(*it)->scan();
 		}
 
-		//copy stuff over?
+		for(Chunk * c = m_pools[m_cur_pool]; c; c = c->m_next)
+		{
+			for(Data * d = c->begin(); d != c->end(); d += d->m_count + sizeof(Data))
+			{
+				switch(d->m_type)
+				{
+				case GC_STRING:  gc_scan( (String*) (d->m_data)); break;
+				case GC_TUPLE:   gc_scan( (String*) (d->m_data)); break;
+				case GC_MESSAGE: gc_scan( (String*) (d->m_data)); break;
+				case GC_CALLABLE_CONTEXT: gc_scan( (CallableContext*) (d->m_data)); break;
+				case GC_RUNNABLE_CONTEXT: gc_scan( (RunnableContext*) (d->m_data)); break;
+				case GC_VARIABLE_FRAME:   gc_scan( (VariableFrame*)   (d->m_data)); break;
+				}
+			}
+		}
 
 		m_in_gc = false;
 	}
-
-
 }
+
