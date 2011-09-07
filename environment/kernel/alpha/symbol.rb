04225d443eb655807eb865cb005a617924635284
Graham Batty <graham-storm-git@ript.net>
2011-09-07 04:28:17 -0600

    Re-enabled the ENV constant and fixed some string methods.

--- environment/kernel/alpha/symbol.rb
+++ environment/kernel/alpha/symbol.rb
@@ -15,6 +15,18 @@ class Symbol
     proc { |obj, *args| obj.send(self, *args) }
   end
 
+  def substr(first, last)
+    first = length + first if (first < 0)
+    last = length + last if (last < 0)
+    first = length - 1 if first >= length
+    last = length - 1 if last >= length
+    if (last < first)
+      first, last = last, first
+    end
+    raise ArgumentError.new("Invalid argument to substr.") if (first < 0 || last < 0)
+    substr(first, last)
+  end
+
   def +(other)
     self + other.to_s_prim
   end
