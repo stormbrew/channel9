7c971331f52cc99fe958e1aa174bfe21652fa4d1
Megan Batty <megan@stormbrew.ca>
2011-10-13 04:12:23 -0600

    Make IStreams collectable.

--- ext/context.hpp
+++ ext/context.hpp
@@ -2,6 +2,7 @@
 
 #include "channel9.hpp"
 #include "environment.hpp"
+#include "callable_context.hpp"
 #include "istream.hpp"
 #include "memory_pool.hpp"
 #include "variable_frame.hpp"
@@ -12,38 +13,6 @@
 
 namespace Channel9
 {
-	class CallableContext
-	{
-	private:
-		typedef std::vector<CallableContext*> context_set;
-		static context_set contexts;
-		static bool sweep_flag;
-
-		bool m_sweep;
-	
-	public:
-		CallableContext() : m_sweep(sweep_flag) 
-		{ contexts.push_back(this); }
-
-		static void sweep();
-
-		virtual void send(Environment *env, const Value &val, const Value &ret) = 0;
-		virtual void scan(); // Make SURE to call down to this.
-		virtual ~CallableContext() {};
-	};
-
-	inline void gc_scan(CallableContext *ctx)
-	{
-		ctx->scan();
-	}
-
-	// Specialize GCRef's scanner to only do a gc_scan of it.
-	template <>
-	inline void GCRef<CallableContext*>::scan()
-	{
-		gc_scan(m_val);
-	}
-
 	struct RunnableContext
 	{
 		IStream *m_instructions;
