a5ff283c430989ea560b580ee2de04d39bb779ad
Megan Batty <megan@stormbrew.ca>
2011-10-22 14:43:55 -0600

    Move trace_out_header to not be inline as it's too big.

---
+++ ext/trace.cpp
@@ -0,0 +1,55 @@
+#include "trace.hpp"
+#include <stdio.h>
+
+namespace Channel9
+{
+	bool trace_mute = false;
+
+	void trace_out_header(int facility, int level, const char * file, int line)
+	{
+		static const char
+	//		*black     = "\033[30m",
+			*red       = "\033[31m",
+			*green     = "\033[32m",
+			*yellow    = "\033[33m",
+			*blue      = "\033[34m",
+	//		*purple    = "\033[35m",
+			*cyan      = "\033[36m",
+			*white     = "\033[37m",
+	//		*bold      = "\033[1m",
+	//		*underline = "\033[4m",
+			*reset     = "\033[0m";
+
+		static const char * levelcolor[] = { white, white, green, yellow, red, red };
+
+		static bool color = isatty(2); //stderr
+
+		if(color) ctrace << cyan;
+		switch(facility)
+		{
+			case TRACE_GENERAL: ctrace << "general";   break;
+			case TRACE_VM:      ctrace << "vm";        break;
+			case TRACE_GC:      ctrace << "gc";        break;
+			default: ctrace << "unknown " << facility; break;
+		}
+		if(color) ctrace << reset;
+
+		ctrace << ".";
+
+		if(color) ctrace << levelcolor[level];
+		switch(level)
+		{
+			case TRACE_SPAM:  ctrace << "spam";     break;
+			case TRACE_DEBUG: ctrace << "debug";    break;
+			case TRACE_INFO:  ctrace << "info";     break;
+			case TRACE_WARN:  ctrace << "warn";     break;
+			case TRACE_ERROR: ctrace << "error";    break;
+			case TRACE_CRIT:  ctrace << "critical"; break;
+			default:          ctrace << "unknown";  break;
+		}
+
+		if(color) ctrace << blue;
+		ctrace << " " << file << ":" << line << ": "; \
+		if(color) ctrace << reset;
+	}
+}
