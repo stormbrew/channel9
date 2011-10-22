25b1dedc81e4df78bf8d2d673321fd8d4b2bb005
Megan Batty <megan@stormbrew.ca>
2011-10-21 23:26:35 -0600

    Enable -Winline to get errors when something can't be inlined.

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -6,7 +6,7 @@ channel9ext = "channel9ext"
 collector = with_config("collector") || 'semispace'
 collector_class = collector.capitalize
 
-$CFLAGS = "-Wall -Werror -frtti " # -Winline"
+$CFLAGS = "-Wall -Werror -frtti -Winline "
 $CFLAGS << "-DCOLLECTOR=" + collector + " -DCOLLECTOR_CLASS=" + collector_class
 $LDFLAGS = "-lstdc++"
 
@@ -38,7 +38,7 @@ if enable_config("debug")
 else
   $CFLAGS << " -DNDEBUG -O4 -g" # --param inline-unit-growth=100000 --param large-function-growth=100000 --param max-inline-insns-single=100000"
 end
-if !enable_config("valgrind")
+if !enable_config("valgrind-client")
   $CFLAGS << " -DNVALGRIND"
 end
 
