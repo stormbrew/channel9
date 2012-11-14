f6e6161f58f1e5b91ef24c68dd267e74a87923d7
Megan Batty <megan@stormbrew.ca>
2012-11-14 02:40:37 -0700

    Eliminate a bunch of very dead code.

--- lib/channel9/ruby.rb
+++ lib/channel9/ruby.rb
@@ -1,6 +1,49 @@
-require 'channel9/ruby/ruby_object'
-require 'channel9/ruby/ruby_class'
-require 'channel9/ruby/ruby_module'
-require 'channel9/ruby/tuple'
-require 'channel9/ruby/unwinder'
+require 'ruby_parser'
+
+require 'channel9'
 require 'channel9/ruby/compiler'
+
+module Channel9
+  module Ruby
+    def self.compile_string(type, str, filename = "__eval__", line = 1)
+      stream = Channel9::Stream.new
+      stream.build do |builder|
+        parser = RubyParser.new
+        begin
+          tree = parser.parse(str, filename)
+          tree = s(type.to_sym, tree)
+          tree.file = filename
+          tree.line = line
+          compiler = Channel9::Ruby::Compiler.new(builder)
+          compiler.transform(tree)
+        rescue Racc::ParseError => e
+          puts "parse error in #{filename}: #{e}"
+          return nil
+        rescue ArgumentError => e
+          puts "argument error in #{filename}: #{e}"
+          return nil
+        rescue SyntaxError => e
+          puts "syntax error in #{filename}: #{e}"
+          return nil
+        rescue NotImplementedError => e
+          puts "not implemented error in #{filename}: #{e}"
+          return nil
+        rescue RegexpError => e
+          puts "invalid regex error in #{filename}: #{e}"
+          return nil
+        end
+      end
+      return stream
+    end
+
+    def self.compile(filename)
+      begin
+        File.open("#{filename}", "r") do |f|
+          return compile_string(:file, f.read, filename)
+        end
+      rescue Errno::ENOENT, Errno::EISDIR
+        return nil
+      end
+    end
+  end
+end
