b37dd72fd09c0a556c895b53740f46b1ad99dbc0
Graham Batty <graham-storm-git@ript.net>
2012-11-17 05:08:46 -0700

    Made rakefile more friendly to cmake build environment.

--- bin/simple_test_runner.rb
+++ bin/simple_test_runner.rb
@@ -8,13 +8,22 @@ if !dir || !File.directory?(dir)
   raise "No directory specified or directory not found (#{dir})."
 end
 
+if ENV['C9RB_PATH']
+  C9RB_PATH = ENV['C9RB_PATH']
+else
+  # add a likely location for the cmake files to the path
+  # so this is more likely to work
+  ENV['PATH'] = ENV['PATH'] + ':../channel9.build/bin'
+  C9RB_PATH = 'c9.rb'
+end
+
 Dir["#{dir}/*.rb"].sort.each do |test|
   if (match = test.match(%r{^#{dir}/([0-9]{3,3})\.(.+)\.rb$}))
     num = match[1]
     name = match[2]
 
     stime = Time.now
-    output = `ruby -rubygems -Ilib -I../channel9/lib bin/c9.rb #{test}`
+    output = `#{C9RB_PATH} #{test}`
     c9_time = Time.now - stime
     total_c9_time += c9_time
 
