0af9bf58451f062a17f1ff99d0344e4fe5bd0883
Megan Batty <megan@stormbrew.ca>
2012-11-19 04:47:28 -0700

    No exception specifications. They mess up backtraces.

--- include/c9/loader.hpp
+++ include/c9/loader.hpp
@@ -13,9 +13,9 @@ namespace Channel9
 	};
 
 	// Loads the bytecode file at the path given. Returns a runnablecontext that can be activated.
-	GCRef<RunnableContext*> load_bytecode(Environment *env, const std::string &filename) throw(loader_error);
+	GCRef<RunnableContext*> load_bytecode(Environment *env, const std::string &filename);
 	// Same as above but rather than loading from the file it loads from the string given.
-	GCRef<RunnableContext*> load_bytecode(Environment *env, const std::string &filename, const std::string &str) throw(loader_error);
+	GCRef<RunnableContext*> load_bytecode(Environment *env, const std::string &filename, const std::string &str);
 
 	// Given the program name and argv, inspects the program name or first argument to determine
 	// which environment to load (based on either the program being c9.ENVNAME or the extension
@@ -26,9 +26,9 @@ namespace Channel9
 	// run in the second argument.
 	// Will also set a trace_loaded special channel in the environment to the value of trace_loaded.
 	// This is to advise the environment that it should unmute tracing before acting on its argv.
-	int load_environment_and_run(Environment *env, std::string program, int argc, const char **argv, bool trace_loaded = false) throw(loader_error);
+	int load_environment_and_run(Environment *env, std::string program, int argc, const char **argv, bool trace_loaded = false);
 
 	// Loads the file at the path given. If environment_loaded is false and the extension is unrecognized
 	// it will attempt to find and load the environment first.
-	int run_file(Channel9::Environment *env, const std::string &filename) throw(loader_error);
+	int run_file(Channel9::Environment *env, const std::string &filename);
 }
