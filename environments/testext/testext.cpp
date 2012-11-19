606a2e0addbc94000a93551d1f2232f52075b620
Graham Batty <graham-storm-git@ript.net>
2012-11-19 05:16:48 -0700

    Work in progress on FFI implementation. Mostly works now.

--- environments/testext/testext.cpp
+++ environments/testext/testext.cpp
@@ -3,6 +3,9 @@
 #include "c9/context.hpp"
 #include "c9/string.hpp"
 #include "c9/value.hpp"
+#include "c9/ffi.hpp"
+
+using namespace Channel9;
 
 class TestChannel : public Channel9::CallableContext
 {
@@ -27,6 +30,43 @@ TestChannel *test_channel = new TestChannel;
 
 extern "C" int Channel9_environment_initialize(Channel9::Environment *env, const std::string &boot_path)
 {
+	FFIDefinition *args = new FFIDefinition("Puts Arguments");
+	args->add_pointer();
+	FFICall *puts_call = new FFICall(ffi_fn(puts), &ffi_type_sint, args);
+
+	FFIDefinition *struct_timespec = new FFIDefinition("struct timespec");
+	struct_timespec->add(ffi_type_map<time_t>()); // 0 time_t tv_sec
+	struct_timespec->add(ffi_type_map<long>()); // 1 time_t tv_nsec
+	FFIDefinition *struct_stat = new FFIDefinition("struct stat");
+	struct_stat->add(ffi_type_map<dev_t>()); // 0 dev_t st_dev
+	struct_stat->add(ffi_type_map<ino_t>()); // 1 ino_t st_ino
+	struct_stat->add(ffi_type_map<nlink_t>()); // 2 nlink_t st_nlink
+	struct_stat->add(ffi_type_map<mode_t>()); // 3 mode_t st_mode
+	struct_stat->add(ffi_type_map<uid_t>()); // 4 uid_t st_uid
+	struct_stat->add(ffi_type_map<gid_t>()); // 5 gid_t st_gid
+	struct_stat->add(ffi_type_map<int>());
+	struct_stat->add(ffi_type_map<dev_t>()); // 6 dev_t st_rdev
+	struct_stat->add(ffi_type_map<off_t>()); // 8 off_t st_size
+	struct_stat->add(ffi_type_map<blksize_t>()); // 9 blksize_t st_blksize
+	struct_stat->add(ffi_type_map<blkcnt_t>()); // 10 blkcnt_t st_blocks
+	struct_stat->add(struct_timespec, 3); // 11-13 time_t st_atim, st_mtime, st_ctim
+/*	struct_stat->add(ffi_type_map<time_t>()); // 11 time_t st_atime
+	struct_stat->add(ffi_type_map<unsigned long>()); // 12 unsigned long st_atime_msec
+	struct_stat->add(ffi_type_map<time_t>()); // 13 time_t st_mtime
+	struct_stat->add(ffi_type_map<unsigned long>()); // 14 unsigned long st_mtime_msec
+	struct_stat->add(ffi_type_map<time_t>()); // 15 time_t st_ctime
+	struct_stat->add(ffi_type_map<unsigned long>()); // 16 unsigned long st_ctime_msec */
+	struct_stat->add(ffi_type_map<int>(), 3);
+	assert(struct_stat->size() == sizeof(struct stat));
+
+	FFIDefinition *stat_args = new FFIDefinition("stat args");
+	stat_args->add_pointer(); // string path
+	stat_args->add_pointer(struct_stat);
+	FFICall *stat_call = new FFICall(ffi_fn(stat), &ffi_type_sint, stat_args);
+
+	env->set_special_channel("ffi_puts", Channel9::value(puts_call));
+	env->set_special_channel("ffi_stat", Channel9::value(stat_call));
+	env->set_special_channel("ffi_struct_stat", Channel9::value(struct_stat));
 	env->set_special_channel("hello_test_channel", Channel9::value(test_channel));
 	return 0;
 }
---
+++ include/c9/ffi.hpp
@@ -0,0 +1,146 @@
+#pragma once
+
+#include "c9/callable_context.hpp"
+#include <ffi.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+
+namespace Channel9
+{
+	template <typename tType>
+	ffi_type *ffi_type_map();
+	template <>
+	inline ffi_type *ffi_type_map<char>() { return &ffi_type_sint8; }
+	template <>
+	inline ffi_type *ffi_type_map<int8_t>() { return &ffi_type_sint8; }
+	template <>
+	inline ffi_type *ffi_type_map<uint8_t>() { return &ffi_type_uint8; }
+	template <>
+	inline ffi_type *ffi_type_map<int16_t>() { return &ffi_type_sint16; }
+	template <>
+	inline ffi_type *ffi_type_map<uint16_t>() { return &ffi_type_uint16; }
+	template <>
+	inline ffi_type *ffi_type_map<int32_t>() { return &ffi_type_sint32; }
+	template <>
+	inline ffi_type *ffi_type_map<uint32_t>() { return &ffi_type_uint32; }
+	template <>
+	inline ffi_type *ffi_type_map<int64_t>() { return &ffi_type_sint64; }
+	template <>
+	inline ffi_type *ffi_type_map<uint64_t>() { return &ffi_type_uint64; }
+
+	class FFIDefinition : public CallableContext
+	{
+		std::string m_name;
+		ffi_type m_type;
+		std::vector<ffi_type*> m_elements;
+		std::vector<FFIDefinition*> m_ptr_types;
+		std::vector<FFIDefinition*> m_dependent_definitions;
+
+	public:
+		FFIDefinition(const std::string &name) : m_name(name)
+		{
+			m_elements.push_back(NULL); // arguments are null terminated
+			m_type.size = 0;
+			m_type.alignment = 0;
+			m_type.type = FFI_TYPE_STRUCT;
+			m_type.elements = &m_elements[0];
+		}
+		FFIDefinition() : m_name("<unknown>")
+		{
+			m_elements.push_back(NULL); // arguments are null terminated.
+			m_type.size = 0;
+			m_type.alignment = 0;
+			m_type.type = FFI_TYPE_STRUCT;
+			m_type.elements = &m_elements[0];
+		}
+
+		size_t size()
+		{
+			normalize();
+			return m_type.size;
+		}
+		size_t count() const { return m_elements.size() - 1; }
+
+		ffi_type *get_type() { return &m_type; }
+		ffi_type const *get_type() const { return &m_type; }
+		ffi_type *get_type(size_t idx) const;
+		FFIDefinition *get_ptr_type(size_t idx) const;
+		size_t get_offset(size_t idx) const;
+
+		void normalize();
+		void add(ffi_type *type, size_t count = 1);
+		void add(FFIDefinition *type, size_t count = 1);
+		void add_pointer(FFIDefinition *type, size_t count = 1);
+		void add_pointer(size_t count = 1); // adds string pointers.
+
+		void send(Environment *env, const Value &val, const Value &ret);
+		void scan();
+		std::string inspect() const { return std::string("FFIDefinition(") + m_name + ")"; }
+
+		~FFIDefinition() {}
+	};
+
+	class FFIObject : public CallableContext
+	{
+		FFIDefinition *m_definition;
+		FFIObject *m_outer; // if it's inside another object.
+		char *m_blob;
+
+	public:
+		FFIObject(FFIDefinition *definition) : m_definition(definition), m_outer(NULL)
+		{
+			m_blob = (char*)malloc(definition->size());
+		}
+		FFIObject(FFIDefinition *definition, FFIObject *outer, char *blob)
+		 : m_definition(definition), m_outer(outer), m_blob(blob)
+		{}
+		~FFIObject()
+		{
+			if (!m_outer)
+				free(m_blob);
+		}
+
+		const FFIDefinition *definition() const { return m_definition; }
+
+		void send(Environment *env, const Value &val, const Value &ret);
+		void scan();
+		std::string inspect() const;
+
+		char *blob() const { return m_blob; }
+
+		void set(size_t idx, const Value &val);
+		Value get(size_t idx) const;
+
+		void raw_set(size_t idx, const char *val);
+		std::pair<const char*, ffi_type*> raw_get(size_t idx);
+	};
+
+	typedef void (*ffi_fn)(void);
+
+	class FFICall : public CallableContext
+	{
+		ffi_fn m_func;
+
+		ffi_cif m_cif;
+
+		ffi_type *m_return;
+		FFIDefinition *m_return_def; // only if m_return is a structure.
+
+		FFIDefinition *m_args;
+
+		void build_cif();
+
+	public:
+		FFICall(ffi_fn func, ffi_type *ret, FFIDefinition *args)
+		 : m_func(func), m_return(ret), m_return_def(NULL), m_args(args)
+		{ build_cif(); }
+		FFICall(ffi_fn func, FFIDefinition *ret, FFIDefinition *args)
+		 : m_func(func), m_return(ret->get_type()), m_return_def(NULL), m_args(args)
+		{ build_cif(); }
+
+		void send(Environment *env, const Value &val, const Value &ret);
+		void scan();
+		std::string inspect() const;
+	};
+}
