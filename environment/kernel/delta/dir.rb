aa695b280201b36bc59a110672055f60994cb52f
Graham Batty <graham-storm-git@ript.net>
2012-11-20 06:50:02 -0700

    Add chdir primitive.

--- environment/kernel/delta/dir.rb
+++ environment/kernel/delta/dir.rb
@@ -2,6 +2,9 @@ module Dir
   def self.pwd
     "." # TODO: Make not a stub.
   end
+  def self.chdir(path)
+    $__c9_ffi_chdir.call(path.to_s_prim)
+  end
   def self.glob(names)
     $__c9_glob.call(names.to_s_prim).collect {|i| i.to_s }.to_a
   end
