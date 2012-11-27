a8b48df37b4f19d8c320be50e607fc95a953b4fe
Megan Batty <megan@stormbrew.ca>
2012-11-27 05:53:52 -0700

    Also forgot to update c9c location here.

--- travis/run-tests.sh
+++ travis/run-tests.sh
@@ -4,5 +4,5 @@ set -e
 # This is just a hackjob for now. Will make it better later.
 
 bin/c9 sample/bytecode/gcd-1mil.c9b
-ruby/bin/c9c sample/c9script/gcd.c9s
+bin/c9c sample/c9script/gcd.c9s
 bin/c9 sample/c9script/gcd.c9b
