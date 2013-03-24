cc9ed907f09b1a57a0d4308b532004822629fb69
Megan Batty <megan@stormbrew.ca>
2013-03-24 05:27:09 -0600

    Implemented gsub.

--- environments/ruby/environment/kernel/delta/string.rb
+++ environments/ruby/environment/kernel/delta/string.rb
@@ -5,8 +5,17 @@ class String
     sprintf(self, *vals)
   end
 
-  def match(*args)
-    # TODO: Make not a stub (soon).
-    nil
+  def match(pattern, pos)
+    if (!pattern.is_a? Regexp)
+      pattern = Regexp.new(pattern)
+    end
+    pattern.match(self, pos)
+  end
+
+  def gsub(pattern, replacement, &block)
+    if (!pattern.is_a? Regexp)
+      pattern = Regexp.new(pattern)
+    end
+    pattern.c9_gsub(self, replacement, &block)
   end
 end
