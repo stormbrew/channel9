e134423ecc3050a3750650bab2697b77879cf380
Megan Batty <megan@stormbrew.ca>
2013-03-25 19:06:57 -0600

    Implemented String#sub

--- environments/ruby/environment/kernel/delta/string.rb
+++ environments/ruby/environment/kernel/delta/string.rb
@@ -18,4 +18,25 @@ class String
     end
     pattern.c9_gsub(self, replacement, &block)
   end
+  def sub(pattern, replacement, &block)
+    if (!pattern.is_a? Regexp)
+      pattern = Regexp.new(pattern)
+    end
+    match = pattern.match(self)
+    if match
+      s = to_s_prim
+      replacement = (replacement || block.call(match[0])).to_s
+      if match.begin(0) > 0
+        s = s.substr(0, match.begin(0)-1) + replacement
+      else
+        s = replacement
+      end
+      if match.end(0) < s.length
+        s += s.substr(match.end(0), s.length - 1)
+      end
+      String.new(s)
+    else
+      self
+    end
+  end
 end
