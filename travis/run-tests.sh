c2b5025ab19496d2d8cbc73abecbd149d00eceef
Graham Batty <graham@stormbrew.ca>
2014-05-14 16:07:26 -0600

    Put it in a different spot.

--- travis/run-tests.sh
+++ travis/run-tests.sh
@@ -4,13 +4,6 @@ set -e
 # This is just a hackjob for now. Will make it better later.
 export PATH=${PATH}:${PWD}/build/bin:${PWD}/bin
 
-# Make libffi pkgconfig setup available.
-[ "${TRAVIS_OS_NAME}" = "osx" ] && {
-	brew --prefix libffi
-	ls $(brew --prefix libffi)/lib/pkgconfig
-	export PKG_CONFIG_PATH="$(brew --prefix libffi)/lib/pkgconfig/":$PKG_CONFIG_PATH
-}
-
 c9 sample/bytecode/gcd-1mil.c9b
 c9 sample/c9script/gcd.c9s
 
