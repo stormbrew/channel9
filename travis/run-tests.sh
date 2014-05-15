8d0c7b72385cab28f0d31ab80bcf6e46b0ee37dc
Megan Batty <megan@stormbrew.ca>
2014-05-14 18:48:11 -0600

    Try forcing LD_LIBRARY_PATH

--- travis/run-tests.sh
+++ travis/run-tests.sh
@@ -5,6 +5,7 @@ rvm use ${RUBY_VERSION}
 
 # This is just a hackjob for now. Will make it better later.
 export PATH=${PATH}:${PWD}/build/bin:${PWD}/bin
+export LD_LIBRARY_PATH=$(ruby -e 'require "rbconfig"; puts RbConfig::CONFIG["prefix"]'):${LD_LIBRARY_PATH}
 
 c9 sample/bytecode/gcd-1mil.c9b
 c9 sample/c9script/gcd.c9s
