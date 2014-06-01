3beab83c17c132f4ab5e33967edf57f88fd66fb0
Graham Batty <graham@stormbrew.ca>
2014-06-01 04:28:43 -0400

    Implemented is_a? for modules.

--- environments/ruby/simple_tests/010.module.rb
+++ environments/ruby/simple_tests/010.module.rb
@@ -20,14 +20,27 @@ test = Y.new
 test.blah(1,2)
 test.blorp(3,4)
 
+puts test.is_a? Enumerable
+puts test.is_a? Z
+
 module K
   def wat
     puts "zomg"
   end
 end
 
+puts test.is_a? K
+
 class Y
   include K
 end
 
+puts test.is_a? K
+
 test.wat
+
+class T < Y
+end
+
+puts Y.new.is_a? Z
+puts Y.new.is_a? K
