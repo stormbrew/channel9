9237d56cc3b15edaa56761a41c531dda540164fa
Megan Batty <megan@stormbrew.ca>
2014-06-01 04:28:43 -0400

    Implemented is_a? for modules.

--- environments/ruby/simple_tests/009.class.rb
+++ environments/ruby/simple_tests/009.class.rb
@@ -10,6 +10,22 @@ class X
 end
 X.new(1).stuff(2)
 
+puts X.new(1).is_a? X
+puts X.new(1).is_a? Object
+puts X.new(1).is_a? Array
+
+class Y < X
+  def stuff(a)
+    puts "sub", @x, a
+  end
+end
+Y.new(2).stuff(3)
+
+puts Y.new(2).is_a? Y
+puts Y.new(2).is_a? X
+puts Y.new(2).is_a? Object
+puts Y.new(2).is_a? Array
+
 x = X.new(2)
 class <<x
   def bloop
